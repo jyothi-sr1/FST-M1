f0d7878ebd26d6d78c1e42555ae7a8cd9df9fc61
jyothi-sr1 <jyothi.sr2@gmail.com>
2025-01-07 12:20:20 +0530

    Add files via upload

---
+++ SQL/Activities/New Text Document.sql
@@ -0,0 +1,10 @@
+--Activity 1
+--Create salesman table
+
+CREATE TABLE salesman (
+    salesman_id int, 
+    salesman_name varchar2(32), 
+    salesman_city varchar2(32), 
+    commission int);
+
+DESC salesman;
